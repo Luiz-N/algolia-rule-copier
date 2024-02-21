0fb63a51ecbb105493557fbba66d12beb488d004
Luiz <lneves@6river.com>
2024-02-21 11:53:03 -0600

    fix: add auth redirect_uri param

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,17 +1,25 @@
 import NextAuth from "next-auth";
 import GitHubProvider from "next-auth/providers/github";
 
+const isProduction = process.env.VERCEL_ENV === "production";
+
 export default NextAuth({
   providers: [
     GitHubProvider({
       clientId: process.env.GITHUB_ID!,
       clientSecret: process.env.GITHUB_SECRET!,
+      authorization: {
+        params: {
+          redirect_uri: isProduction
+            ? "https://algolia-rule-copier.vercel.app/api/auth/callback/github"
+            : "http://localhost:3000/api/auth/callback/github",
+        },
+      },
     }),
   ],
   callbacks: {
     async session({ session, token, user }) {
       console.log({ session, token, user });
-      // Send properties to the client, like an access_token from a provider.
       return session;
     },
   },
